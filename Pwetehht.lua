2c3b1c10c42328c1c3cc2831ac89e027e5bb2328
DayyGT <102882855+DayyGT@users.noreply.github.com>
2026-03-03 13:02:52 +0700

    Delete Pwetehht.lua

--- Pwetehht.lua
+++
@@ -1,4 +0,0 @@
-LogToConsole("`bScript belum dapat dipakai!")
-LogToConsole("`cTunggu sebentar.. sedang proses perbaikan fitur")
-LogToConsole("`2Orang sabar di-sayang Tuhan (kata adminku)")
-LogToConsole("`5Untuk info lebih lanjut, silahkan cek di discord")
